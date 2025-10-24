948df4ba915bfa54d1744b3ff1e0078fe1313b4b
Zakaria Ennaqui <zakariaennaqui.pro@gmail.com>
2025-10-24 01:34:57 +0100

    Delete Application ensab + guides/ensab_v2 directory

--- Application ensab + guides/ensab_v2/blindsqli/auth/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION = [];
-session_destroy();
-session_unset();
-header("location: login.php");
-?>
--- Application ensab + guides/ensab_v2/includes/display_errors.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-ini_set('display_errors', 'off');
-
-error_reporting(0);
-
-?>
